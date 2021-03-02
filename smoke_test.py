d52dbe62d59869789a4e76c5ee6b10b61304bb95
Cariad Eccleston <cariad@hey.com>
2021-03-02 10:18:51 +0000

    Add smoke_test.py

---
+++ smoke_test.py
@@ -0,0 +1,4 @@
+from progrow import Row, Style
+row = Row("foo", maximum=10, current=3)
+style = Style(color=False, show_fraction=True, show_percent=True, width=30)
+assert row.render(style=style) == "foo ███▉          3 / 10 • 30%"
